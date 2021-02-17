ce5352c780f3c8e36b3f9bcb004eb7a3a39329d6
QueenArizona <queen483arizona@gmail.com>
2021-02-17 15:37:39 +0300

    render: gamefield
    Rendered gamefield with it borders.
    Add unit test for calcTileType function.

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -5,8 +5,7 @@ export default class GameController {
   }
 
   init() {
-    // TODO: add event listeners to gamePlay events
-    // TODO: load saved stated from stateService
+    this.gamePlay.drawUi('prairie');
   }
 
   onCellClick(index) {
