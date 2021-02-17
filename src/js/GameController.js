054a6521d874ae9a75b0743fe6c4205d9dbc758c
QueenArizona <queen483arizona@gmail.com>
2021-02-17 16:14:01 +0300

    add: select characters logic
    Added visual response for selected characters and showing error for unable choice.

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -4,6 +4,7 @@ import Magician from './characters/Magician';
 import Undead from './characters/Undead';
 import Vampire from './characters/Vampire';
 import Daemon from './characters/Daemon';
+import GamePlay from './GamePlay';
 import { generateTeam } from './generators';
 import info from './info';
 
@@ -14,6 +15,8 @@ export default class GameController {
     this.userTeam = generateTeam([Bowman, Swordsman], 1, 2);
     this.computerTeam = generateTeam([Undead, Daemon, Vampire], 1, 2);
     this.players = [...this.userTeam, ...this.computerTeam];
+    this.selectedCell = null;
+    this.selectedChar = null;
   }
 
   init() {
@@ -25,7 +28,20 @@ export default class GameController {
   }
 
   onCellClick(index) {
-    // TODO: react to click
+    const characterOnCell = this.players.find((el) => el.position === index);
+
+    if (characterOnCell) {
+      if (['bowman', 'swordsman', 'magician'].includes(characterOnCell.character.type)) {
+        this.gamePlay.selectCell(index);
+        if (this.selectedCell != null) {
+          this.gamePlay.deselectCell(this.selectedCell);
+        }
+        this.selectedCell = index;
+        this.selectedChar = characterOnCell;
+      } else {
+        GamePlay.showError('You cannot play this character');
+      }
+    }
   }
 
   onCellEnter(index) {
