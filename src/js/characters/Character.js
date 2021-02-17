3fc868d6b2942eeac949083b7d618ffc7f6524bd
QueenArizona <queen483arizona@gmail.com>
2021-02-17 15:54:04 +0300

    done: characters creation logic
    Added preventing the creation of characters by "new Character()".
    Added unit test for Character.
    Added children types.

--- src/js/characters/Character.js
+++ src/js/characters/Character.js
@@ -1,10 +1,10 @@
 export default class Character {
   constructor(level, type = 'generic') {
     this.level = level;
-    this.attack = 0;
-    this.defence = 0;
     this.health = 50;
     this.type = type;
-    // TODO: throw error if user use "new Character()"
+    if (new.target === Character) {
+      throw Error('You cannot create new object of Character. Please, use inherited classes');
+    }
   }
 }
