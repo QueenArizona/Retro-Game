7e404305a30e7c24a69253220fb4ce4de97dc852
QueenArizona <queen483arizona@gmail.com>
2021-02-17 16:01:25 +0300

    render teams
    Draw characters on their positions

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,11 +1,23 @@
+import Bowman from './characters/Bowman';
+import Swordsman from './characters/Swordsman';
+import Magician from './characters/Magician';
+import Undead from './characters/Undead';
+import Vampire from './characters/Vampire';
+import Daemon from './characters/Daemon';
+import { generateTeam } from './generators';
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.userTeam = generateTeam([Bowman, Swordsman], 1, 2);
+    this.computerTeam = generateTeam([Undead, Daemon, Vampire], 1, 2);
+    this.players = [...this.userTeam, ...this.computerTeam];
   }
 
   init() {
     this.gamePlay.drawUi('prairie');
+    this.gamePlay.redrawPositions(this.players);
   }
 
   onCellClick(index) {
