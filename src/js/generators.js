36303029c14a2dc4a57eb2b218e2ecef30b46fbf
QueenArizona <queen483arizona@gmail.com>
2021-02-17 15:46:04 +0300

    Done: team generation
    Implemented characterGenerator and generateTeam function.
    Add function that generates position.
    Fixed eslint errors.
    Moved Character and PositionCharacter classes to separate folder.

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,6 @@
+import getPosition from './getPosition';
+import PositionedCharacter from './characters/PositionedCharacter';
+
 /**
  * Generates random characters
  *
@@ -6,9 +9,27 @@
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+  while (true) {
+    const randomLevel = Math.floor(Math.random() * maxLevel) + 1;
+    const RandomType = allowedTypes[Math.floor(Math.random() * allowedTypes.length)];
+
+    yield new RandomType(randomLevel);
+  }
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+  const team = [];
+  const positions = [];
+
+  for (let i = 0; i < characterCount; i += 1) {
+    const character = characterGenerator(allowedTypes, maxLevel).next().value;
+    let position = getPosition(character);
+
+    while (positions.includes(position)) {
+      position = getPosition(character);
+    }
+    positions.push(position);
+    team.push(new PositionedCharacter(character, position));
+  }
+  return team;
 }
